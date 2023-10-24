22b2b42bb25b0013755a44f929a8ff96d334b2fb
Chaz Coats <chazcoats2@hotmail.com>
2023-10-24 11:23:35 -0400

    working on routing for different pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ReactFullpage from '@fullpage/react-fullpage';
-import './index.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
+import App from "./app";
+import AboutMe from "./pages/aboutme";
+import Contact from "./pages/contact";
+import Portfolio from "./pages/portfolio";
+import Resume from "./pages/resume";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <AboutMe />,
+            },
+            {
+                path: "/portfolio",
+                element: <Portfolio />,
+            },
+            {
+                path: "/resume",
+                element: <Resume />,
+            },
+            {
+                path: "/contact",
+                element: <Contact />,
+            }
+        ]
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById("react-root")).render(
+    <RouterProvider router={router} />
+);
--- src/pages/aboutme.jsx
+++ src/pages/aboutme.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./aboutmeStyles.css";
 
 export default class AboutMe extends React.Component {
     render() {
